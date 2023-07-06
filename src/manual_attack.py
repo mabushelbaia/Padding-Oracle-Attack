7a63b79598b43c0f35c439fda463abdbd50a6498
Mohammad Abu-Shelbaia <mabushelbaia@localhost.localdomain>
2023-07-06 16:05:47 +0300

    OKEY

--- src/manual_attack.py
+++ src/manual_attack.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 import socket
 from binascii import hexlify, unhexlify
+from time import sleep
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # XOR two bytearrays
 def xor(first, second):
@@ -31,44 +43,93 @@ class PaddingOracle:
 
 
 if __name__ == "__main__":
-    oracle = PaddingOracle('10.9.0.80', 6000)
-    IV, C1, C2, C3 = oracle.ctext[:16], oracle.ctext[16:32], oracle.ctext[32:48], oracle.ctext[48:]
-    P = bytearray(48)
-    C2_copy, C1_copy, IV_copy = bytearray(16), bytearray(16), bytearray(16)
+    oracle = PaddingOracle('10.9.0.80', 5000)
+    C = oracle.ctext
+    cipher_size = len(C)
+    plain_size = cipher_size - 16
+    P = bytearray(plain_size)
+    D = xor(P, C)
+    C_copy = bytearray(plain_size)
+    
     for k in range(1, 17):
         for i in range(256):
-            C2_copy[16 - k] = i
-            status = oracle.decrypt(IV + C1 + C2_copy + C3)
-            if status == "Valid":
+            C_copy[plain_size - k] = i
+            if oracle.decrypt(C[:16] + C_copy[16:32] + C[32:]) == "Valid":
                 break
+        P[plain_size - k] = C[plain_size - k] ^ C_copy[plain_size - k] ^ k
+        for j in range(1, k+1):
+            C_copy[plain_size - j] = C[plain_size - j] ^ (k + 1) ^ P[plain_size - j]
 
-        P[48 - k] = C2_copy[16 - k] ^ C2[16 - k] ^ k
-        for j in range(1, k+1):
-            C2_copy[16 - j] = C2[16 - j] ^ (k + 1) ^ P[48 - j]
-        
-        for i in range(256):
-            C1_copy[16 - k] = i
-            status = oracle.decrypt(IV + C1_copy + C2)
-            if status == "Valid":
-                break
+        # Print the updated P with changed digit in green
+#!/usr/bin/python3
+import socket
+from binascii import hexlify, unhexlify
+from time import sleep
 
-        P[32 - k] = C1_copy[16 - k] ^ C1[16 - k] ^ k
-        for j in range(1, k+1):
-            C1_copy[16 - j] = C1[16 - j] ^ (k + 1) ^ P[32 - j]
-        
-        for i in range(256):
-            IV_copy[16 - k] = i
-            status = oracle.decrypt(IV_copy + C1)
-            if status == "Valid":
-                break
-        P[16 - k] = IV_copy[16 - k] ^ IV[16 - k] ^ k
-        for j in range(1, k+1):
-            IV_copy[16 - j] = IV[16 - j] ^ (k + 1) ^ P[16 - j]
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# XOR two bytearrays
+def xor(first, second):
+   return bytearray(x^y for x,y in zip(first, second))
+
+class PaddingOracle:
+
+    def __init__(self, host, port) -> None:
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((host, port))
+
+        ciphertext = self.s.recv(4096).decode().strip()
+        self.ctext = unhexlify(ciphertext)
+
+    def decrypt(self, ctext: bytes) -> None:
+        self._send(hexlify(ctext))
+        return self._recv()
+
+    def _recv(self):
+        resp = self.s.recv(4096).decode().strip()
+        return resp 
+
+    def _send(self, hexstr: bytes):
+        self.s.send(hexstr + b'\n')
+
+    def __del__(self):
+        self.s.close()
+
+
+if __name__ == "__main__":
+    oracle = PaddingOracle('10.9.0.80', 5000)
+    C = oracle.ctext
+    cipher_size = len(C)
+    plain_size = cipher_size - 16
+    P = bytearray(plain_size)
+    D = xor(P, C)
+    C_copy = bytearray(plain_size)
     
-    print(f"[{len(IV)} Bytes] IV:  {IV.hex()}")
-    print(f"[{len(C1)} Bytes] C1:  {C1.hex()}")
-    print(f"[{len(C2)} Bytes] C2:  {C2.hex()}")        
-    print(f"[{len(P)//3} Bytes] P1:  {P[00:16].hex()}")
-    print(f"[{len(P)//3} Bytes] P2:  {P[16:32].hex()}")
-    print(f"[{len(P)//3} Bytes] P3:  {P[32:48].hex()}")
-    print(f"String: {P.decode()}")
+    for k in range(1, plain_size + 1):
+        for i in range(256):
+            C_copy[plain_size - k] = i
+            if oracle.decrypt(C[:16] + C_copy[16:32] + C[32:]) == "Valid":
+                break
+        P[plain_size - k] = C[plain_size - k] ^ C_copy[plain_size - k] ^ k
+        for j in range(1, k+1):
+            C_copy[plain_size - j] = C[plain_size - j] ^ (k + 1) ^ P[plain_size - j]
+
+        # Print the updated P with changed digit in green
+        colored_P = ""
+        for index, digit in enumerate(P):
+            if digit != 0x00:
+                colored_P += bcolors.OKGREEN + f"{digit:02X}" + bcolors.ENDC
+            else:
+                colored_P += f"{digit:02X}"
+
+        print(f"[PLAIN] {colored_P}\r", end="")
+        sleep(0.1)
